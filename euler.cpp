ec2e111fbd738d2038fa95bdd79cba7af945876b
Alex Jolicoeur <ajolicoe@ucsc.edu>
2013-03-18 19:16:51 -0700

    minor change to euler.cpp

--- euler.cpp
+++ euler.cpp
@@ -141,10 +141,10 @@ int euler9 () {
 }
 
 bool isPrime (int num) {
-  for (int i = 2; i <= sqrt(num); i++) {
-	if (num % i == 0) return false;
-  }
-  return true;
+	for (int i = 2; i <= sqrt(num); i++) {
+		if (num % i == 0) return false;
+	}
+	return true;
 }
 
 long long euler10 () {
